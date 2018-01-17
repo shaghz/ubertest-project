cb01a0337137d775415aef43db683a0762622d8e
shaghz <sh.khangaldy@gmail.com>
2018-01-17 16:17:28 +0330

    added new slideshow with my jq file

--- slider.js
+++ slider.js
@@ -1,52 +1,41 @@
+ $(document).ready(function(){
 
-$(document).ready(function () {
-    $('#driver-slideshow').slick({
+    var x ,
+    $prev = $('#my-prev'),
+    $next =$('#my-next'),
+    $slideshow = $('#driver-slideshow');
 
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        variableWidth: true,
-        touchMove:false,
-        prevArrow: '<button type="button" id="my-prev" class="my-prev hidebutton"><span class=" uk-icon" uk-icon="icon: arrow-left;ratio:1.8"></span></button>',
-        nextArrow: '<button type="button" class="my-next"><span class=" uk-icon" uk-icon="icon: arrow-right;ratio:1.8"></span></button>'
+    function getPercentage(){
+        var totalWidth = 0;
+        $('.s-card').each(function() {
+            totalWidth += $(this).width() ;
+        });
+        x = -totalWidth / 50 + '%';
+        console.log(x);
+    };
+    function showNext(){
+        $next.show();
+        $prev.hide();
+        $('.toggle').toggleClass('slider-button active-slider-button');
 
+    };
+    function showPrev(){
+        $next.hide();
+        $prev.show();
+        $('.toggle').toggleClass('slider-button active-slider-button');
+    };
+
+    getPercentage(x);
+    $prev.hide();
+
+    $('#my-next,#slider-right-button').on('click',function(){
+        showPrev();
+        $slideshow.css("transform","translate("+ x +" ,0)");
     });
-    $('.my-next').click(function() {
-        $("#driver-slideshow").addClass("driver-slideshow-2").removeClass("driver-slideshow-1");
-        $(".slideshow-padding-1").addClass("slideshow-padding-2").removeClass("slideshow-padding-1");
-        $("#my-prev").addClass("blockbutton").removeClass("hidebutton");
-        $('.my-next').hide();
-        $('.slick-dots').css("left" ,"400px")
 
-    })
-    $('#slick-slide-control01').click(function() {
-        $("#driver-slideshow").addClass("driver-slideshow-2").removeClass("driver-slideshow-1");
-        $(".slideshow-padding-1").addClass("slideshow-padding-2").removeClass("slideshow-padding-1");
-        $("#my-prev").addClass("blockbutton").removeClass("hidebutton");
-        $('.my-next').hide();
-        $('.slick-dots').css("left" ,"400px");
-    })
-    $('#slick-slide-control00').click(function() {
-        $("#driver-slideshow").addClass("driver-slideshow-1").removeClass("driver-slideshow-2");
-        $(".slideshow-padding-2").addClass("slideshow-padding-1").removeClass("slideshow-padding-2");
-        $('.my-next').show();
-        $("#my-prev").removeClass("blockbutton").addClass("hidebutton");
-        $('.slick-dots').css("right" ,"-156px");
-        $('.slick-dots').css("left" ,"0px");
-
-    })
-    $('.my-prev').click(function() {
-
-        $("#driver-slideshow").addClass("driver-slideshow-1").removeClass("driver-slideshow-2");
-        $(".slideshow-padding-2").addClass("slideshow-padding-1").removeClass("slideshow-padding-2");
-        $('.my-next').show();
-        $("#my-prev").removeClass("blockbutton").addClass("hidebutton");
-        $('.slick-dots').css("right" ,"-156px");
-        $('.slick-dots').css("left" ,"0px");
-    })
-
-
-
+    $('#my-prev ,#slider-left-button').on('click',function(){
+        showNext();
+        $slideshow.css("transform","translate(0%,0)");
+    })   ;
 });
+
